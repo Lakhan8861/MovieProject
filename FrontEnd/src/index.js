7a2a020d15c4d4ac608a754ebf18f0ff63ffa912
Lakhan8861 <lr165555@gmail.com>
2024-12-30 18:53:42 +0530

    first commit

---
+++ FrontEnd/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
---
+++ README.md
@@ -0,0 +1,12 @@
+Required tools/
+
+install react js
+install jdk
+install springboot
+install allspringboot library
+install node_modules
+using command npm i
+add your mongodb
+import the json file in mongodb data
+run the springboot
+run the react js using npm start
